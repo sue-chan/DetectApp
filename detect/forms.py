e3d2238f470847738ebf07f984d3f3b17de981c3
satoshi <nemunemu_ya@yahoo.co.jp>
2019-11-14 16:35:58 +0900

    css

--- detect/forms.py
+++ detect/forms.py
@@ -1,5 +1,5 @@
 from django import forms                                                           
-
+from .models import Photo
 
 class PhotoForm(forms.Form):
     image = forms.ImageField()
--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'detect.apps.DetectConfig'
+    'detect.apps.DetectConfig',
 ]
 
 MIDDLEWARE = [
